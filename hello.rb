a8822b838af9f54d3ac3d4b9eb6f6c37a075bbee
Joram Okwaro <joram@kopokopo.com>
2013-07-22 14:35:54 +0300

    fix

--- hello.rb
+++ hello.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'json'
 
 get '/k2_back_office_notification.json' do
+  status 200
   content_type :json
   { :status => '01', :description => 'Accepted', :subscriber_message => "Back office notification received" }.to_json
 end
