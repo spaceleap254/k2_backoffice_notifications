508bd24bbb00e988f033543ad333a429c443d4dc
Joram Okwaro <joram@kopokopo.com>
2013-07-22 14:17:19 +0300

    fix

--- hello.rb
+++ hello.rb
@@ -3,5 +3,5 @@ require 'json'
 
 get '/k2_back_office_notification.json' do
   content_type :json
-  { :status => '01', :description => 'Accepted', : }.to_json
+  { :status => '01', :description => 'Accepted', :subscriber_message => "Back office notification received" }.to_json
 end
