a4f86a02eb3c47cb7a8d3d40ccf187539fdd820d
Joram Okwaro <joram@kopokopo.com>
2013-07-22 18:09:44 +0300

    added index route

--- back_office_notifications_sandbox.rb
+++ back_office_notifications_sandbox.rb
@@ -1,13 +1,17 @@
 require 'sinatra'
 require 'json'
 
-get '/k2_back_office_notification.json' do
+get '/' do
+  "Go to /httppost_v2.json for the JSON response for HTTP POST notifications. More to come..."
+end
+
+get '/httppost_v2.json' do
   status 200
   content_type :json
   { :status => '01', :description => 'Accepted', :subscriber_message => "Back office notification received" }.to_json
 end
 
-post '/k2_back_office_notification.json' do
+post '/httppost_v2.json' do
   status 200
   content_type :json
   { :status => '01', :description => 'Accepted', :subscriber_message => "Back office notification received for transaction #{params[:transaction_reference]} from #{params[:first_name]} #{params[:last_name]}" }.to_json
