bd9a07253466aa79f2d9eb2212f308b5bf4fae14
Joram Okwaro <joram@kopokopo.com>
2013-10-18 18:19:17 +0300

    made the subscriber message conditional

--- back_office_notifications_sandbox.rb
+++ back_office_notifications_sandbox.rb
@@ -5,14 +5,17 @@ get '/' do
   "Go to /httppost_v2.json for the JSON response for HTTP POST notifications. More to come..."
 end
 
-get '/httppost_v2.json' do
-  status 200
-  content_type :json
-  { :status => '01', :description => 'Accepted', :subscriber_message => "Back office notification received" }.to_json
-end
-
 post '/httppost_v2.json' do
   status 200
+  description = "Accepted"
+  reference = params[:transaction_reference]
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+  if !reference.empty? && !first_name.empty? && !last_name.empty?
+  	subscriber_message = "Subscriber message ndio hiyo > Transaction reference: #{params[:transaction_reference]}. Customer: #{params[:first_name]} #{params[:last_name]}."
+  else
+  	subscriber_message = ""
+  end
   content_type :json
-  { :status => '01', :description => 'Accepted', :subscriber_message => "Back office notification received for transaction #{params[:transaction_reference]} from #{params[:first_name]} #{params[:last_name]}" }.to_json
+  { :status => '01', :description => description, :subscriber_message => subscriber_message }.to_json
 end
