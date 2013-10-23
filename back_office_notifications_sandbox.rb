4fa659d1703a48a0cc408e6a6b3207d4989cd131
Joram Okwaro <joram@kopokopo.com>
2013-10-23 17:00:19 +0300

    done

--- back_office_notifications_sandbox.rb
+++ back_office_notifications_sandbox.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-  "Go to /httppost_v2.json for the JSON response for HTTP POST notifications. More to come..."
+  "Go to /httppost_v2.json for HTTP POST notifications and /xml_http_v2.xml for XML over HTTP notifications."
 end
 
 post '/httppost_v2.json' do
@@ -27,25 +27,28 @@ post '/httppost_v2_hmac.json' do
 end
 
 post '/xml_http_v2.xml' do
-
-    description = "Accepted"
-    reference = params[:transaction_reference]
-    first_name = params[:first_name]
-    last_name = params[:last_name]
-    subscriber_message = ""
-    if !reference.nil? && !first_name.nil? && !last_name.nil?
-      subscriber_message = "Subscriber message for XML HTTP V2! Transaction reference: #{reference}. Customer: #{first_name} #{last_name}."
-    end
-    content_type 'text/xml'
+  description = "Accepted"
+  reference = params[:transaction_reference]
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+  subscriber_message = ""
+  if !reference.nil? && !first_name.nil? && !last_name.nil?
+    subscriber_message = "Subscriber message for XML HTTP V2! Transaction reference: #{reference}. Customer: #{first_name} #{last_name}."
+  end
+  content_type 'text/xml'
   "<?xml version = '1.0' encoding = 'UTF-8'?>
-<transaction_response>
-    <internal_transaction_id>3222</internal_transaction_id>
-    <transaction_reference>#{reference}</transaction_reference>
-    <status>01</status>
-    <description>#{description}</description>
-    <subscriber_message>#{subscriber_message}</subscriber_message>
-</transaction_response>"
+     <transaction_response>
+     <internal_transaction_id>3222</internal_transaction_id>
+     <transaction_reference>#{reference}</transaction_reference>
+     <status>01</status>
+     <description>#{description}</description>
+     <subscriber_message>#{subscriber_message}</subscriber_message>
+   </transaction_response>"
 end
+
+post '/xml_http_v2_failed_reconciliation.xml' do
+end
+
 
 
 
