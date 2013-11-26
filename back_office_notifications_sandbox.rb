e28f0d042d910adc00a48231adffccb13b673b5f
Joram Okwaro <joram@kopokopo.com>
2013-11-26 12:01:08 +0300

    added middle name to subscriber message

--- back_office_notifications_sandbox.rb
+++ back_office_notifications_sandbox.rb
@@ -12,9 +12,10 @@ post '/httppost_v2.json' do
   description = "Accepted"
   reference = params[:transaction_reference]
   first_name = params[:first_name]
+  middle_name = params[:middle_name]
   last_name = params[:last_name]
   if !reference.nil? && !first_name.nil? && !last_name.nil?
-  	subscriber_message = "Subscriber message for HTTP POST V2! Transaction reference: #{params[:transaction_reference]}. Customer: #{params[:first_name]} #{params[:last_name]}."
+  	subscriber_message = "Subscriber message for HTTP POST V2! Transaction reference: #{params[:transaction_reference]}. Customer: #{params[:first_name]} #{params[:middle_name]} #{params[:last_name]}."
   else
   	subscriber_message = ""
   end
